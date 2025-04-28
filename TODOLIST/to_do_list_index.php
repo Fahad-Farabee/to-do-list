65c5132980edf115eb0c596a3b2373395a517e25
S.M.Fahad Farabee <124309861+Fahad-Farabee@users.noreply.github.com>
2025-04-29 03:10:49 +0600

    Update to_do_list_index.php

--- TODOLIST/to_do_list_index.php
+++ TODOLIST/to_do_list_index.php
@@ -1,3 +1,8 @@
+<?php
+    require_once 'includes/config_session.inc.php';
+    require_once 'includes/add_task_view.inc.php';
+  
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,5 +12,16 @@
 </head>
 <body>
     <h3>Welcome to TODOLIST</h3>
+    <h4>Add your task</h4>
+    <form action="includes/add_task.inc.php" method="post">
+    <input type="text" name="task" placeholder="Enter your task">
+    <input type="date" name="date">
+    <input type="time" name="time">
+    <button type="submit">Add</button>
+    </form>
+    <?php
+        task_error_massages();
+    ?>
+
 </body>
 </html>
