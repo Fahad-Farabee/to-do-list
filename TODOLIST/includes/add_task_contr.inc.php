a106ef5886967a76316cb88148ceb2ef95aef490
S.M.Fahad Farabee <124309861+Fahad-Farabee@users.noreply.github.com>
2025-04-30 22:04:37 +0600

    Delete TODOLIST/includes directory

--- TODOLIST/includes/add_task_contr.inc.php
+++
@@ -1,9 +0,0 @@
-<?php
-declare(strict_types=1);
-function is_input_empty(string $task, string $date, string $time){
-    if(empty($task) || empty($date) || empty($time)){
-        return true;
-    }else{
-        return false;
-    }
-}
